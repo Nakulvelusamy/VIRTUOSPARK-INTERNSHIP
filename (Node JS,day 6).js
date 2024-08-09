029578f3a2a8e900a6da84ceaea2c0b95d3738cd
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2024-08-09 10:25:40 +0530

    Create (Node JS,day 6).js

---
+++ (Node JS,day 6).js
@@ -0,0 +1,16 @@
+const file=require('fs')  
+let name="Nakul V"
+console.log("Nakul_V");
+let filedata=file.readFileSync('data.txt').toString();
+console.log(filedata);
+file.renameSync('data.txt','filedata.txt')
+let filedata2=file.readFileSync('filedata.txt').toString();
+console.log(filedata2);
+file.writeFileSync('filedata.txt','this is file data wrote by Nakul_V')
+let filedata3=file.readFileSync('filedata.txt').toString();
+console.log(filedata3);
+file.appendFileSync('filedata.txt','hello world2')
+let filedata4=file.readFileSync('filedata.txt').toString();
+console.log(filedata4);
+file.unlinkSync('filedata.txt')
+file.rmdirSync('testfolder')
